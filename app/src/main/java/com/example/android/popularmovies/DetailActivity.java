f30f8211b56d7308ef7008d0f73661acf2443bc6
Amador Cuenca <sphi02ac@gmail.com>
2018-08-18 08:56:11 -0500

    feat: Create Details layout

--- app/src/main/java/com/example/android/popularmovies/DetailActivity.java
+++ app/src/main/java/com/example/android/popularmovies/DetailActivity.java
@@ -3,6 +3,7 @@ package com.example.android.popularmovies;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.example.android.popularmovies.http.MoviesAPI;
