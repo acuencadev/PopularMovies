c7b6b7018e86c646bd02c7c1c21c2b883df82bd0
Amador Cuenca <sphi02ac@gmail.com>
2018-09-24 18:16:14 -0500

    feat: Add database singleton object

---
+++ app/src/main/java/com/example/android/popularmovies/data/database/MoviesDatabase.java
@@ -0,0 +1,33 @@
+package com.example.android.popularmovies.data.database;
+
+import android.arch.persistence.room.Database;
+import android.arch.persistence.room.Room;
+import android.arch.persistence.room.RoomDatabase;
+import android.arch.persistence.room.TypeConverters;
+import android.content.Context;
+
+@Database(entities = { MovieEntry.class }, version = 1)
+@TypeConverters(DateConverter.class)
+public abstract class MoviesDatabase extends RoomDatabase {
+
+    private static final String DATABASE_NAME = "movies";
+
+    private static final Object LOCK = new Object();
+    private static volatile MoviesDatabase sInstance;
+
+    public abstract MoviesDao moviesDao();
+
+    public static MoviesDatabase getsInstance(Context context) {
+        if (sInstance == null) {
+            synchronized (LOCK) {
+                if (sInstance == null) {
+                    sInstance = Room.databaseBuilder(context.getApplicationContext(),
+                            MoviesDatabase.class,
+                            MoviesDatabase.DATABASE_NAME).build();
+                }
+            }
+        }
+
+        return sInstance;
+    }
+}
