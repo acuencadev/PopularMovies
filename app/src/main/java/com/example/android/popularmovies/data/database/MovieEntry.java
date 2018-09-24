0407c4ca26c37d095341e10376611a2e601301d4
Amador Cuenca <sphi02ac@gmail.com>
2018-09-24 18:09:28 -0500

    feat: Add MovieEntry DB POJO

---
+++ app/src/main/java/com/example/android/popularmovies/data/database/MovieEntry.java
@@ -0,0 +1,114 @@
+package com.example.android.popularmovies.data.database;
+
+import android.arch.persistence.room.ColumnInfo;
+import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.PrimaryKey;
+
+@Entity(tableName = "movie")
+public class MovieEntry {
+
+    @PrimaryKey
+    private String id;
+
+    @ColumnInfo(name = "image_path")
+    private String imagePath;
+
+    @ColumnInfo(name = "backdrop_path")
+    private String backdropPath;
+
+    private String year;
+    private String length;
+    private String description;
+    private String genres;
+    private String status;
+
+    private int votes;
+    private int page;
+
+
+    public MovieEntry(String id, String imagePath, String backdropPath, String year, String length, String description,
+                      String genres, String status, int votes, int page) {
+        this.id = id;
+        this.imagePath = imagePath;
+        this.backdropPath = backdropPath;
+        this.year = year;
+        this.length = length;
+        this.description = description;
+        this.genres = genres;
+        this.status = status;
+        this.votes = votes;
+        this.page = page;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getImagePath() {
+        return imagePath;
+    }
+
+    public void setImagePath(String imagePath) {
+        this.imagePath = imagePath;
+    }
+
+    public String getBackdropPath() {
+        return backdropPath;
+    }
+
+    public void setBackdropPath(String backdropPath) {
+        this.backdropPath = backdropPath;
+    }
+
+    public String getYear() {
+        return year;
+    }
+
+    public void setYear(String year) {
+        this.year = year;
+    }
+
+    public String getLength() {
+        return length;
+    }
+
+    public void setLength(String length) {
+        this.length = length;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getGenres() {
+        return genres;
+    }
+
+    public void setGenres(String genres) {
+        this.genres = genres;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public int getVotes() {
+        return votes;
+    }
+
+    public void setVotes(int votes) {
+        this.votes = votes;
+    }
+}
