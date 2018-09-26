c3bdb4ebf07f6026a604669a652762a636da99c7
Amador Cuenca <sphi02ac@gmail.com>
2018-09-26 16:24:44 -0500

    feat: Add Network Model to Data Model converter

---
+++ app/src/main/java/com/example/android/popularmovies/utility/MovieToMovieEntryConverter.java
@@ -0,0 +1,36 @@
+package com.example.android.popularmovies.utility;
+
+import com.example.android.popularmovies.data.database.MovieEntry;
+import com.example.android.popularmovies.data.network.models.Movie;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MovieToMovieEntryConverter {
+
+    public static List<MovieEntry> moviesToMovieEntries(List<Movie> movies, int page) {
+        List<MovieEntry> movieEntries = new ArrayList<>();
+
+        for (Movie movie : movies) {
+            movieEntries.add(moviesToMovieEntries(movie, page));
+        }
+
+        return movieEntries;
+    }
+
+    public static MovieEntry moviesToMovieEntries(Movie movie, int page) {
+        MovieEntry movieEntry = new MovieEntry(
+                Integer.toString(movie.getId()),
+                movie.getPosterPath(),
+                movie.getBackdropPath(),
+                movie.getReleaseDate().substring(0, 4),
+                movie.getRuntimeString(),
+                movie.getTitle(),
+                movie.getGenresString(),
+                movie.getStatus(),
+                movie.getVoteCount(),
+                page);
+
+        return movieEntry;
+    }
+}
