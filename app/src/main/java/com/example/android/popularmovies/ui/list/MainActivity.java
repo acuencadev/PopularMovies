37026508727125af5cd10588724e569bd30da75e
Amador Cuenca <sphi02ac@gmail.com>
2018-09-27 10:03:06 -0500

    refactor: Remove Butterknife and add DataBinding to MainActivity

--- app/src/main/java/com/example/android/popularmovies/ui/list/MainActivity.java
+++ app/src/main/java/com/example/android/popularmovies/ui/list/MainActivity.java
@@ -2,22 +2,17 @@ package com.example.android.popularmovies.ui.list;
 
 import android.arch.lifecycle.ViewModelProviders;
 import android.content.Intent;
-import android.content.SharedPreferences;
+import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.preference.PreferenceManager;
 import android.support.v7.widget.GridLayoutManager;
-import android.support.v7.widget.RecyclerView;
 import android.view.Menu;
 import android.view.MenuItem;
 
 import com.example.android.popularmovies.R;
+import com.example.android.popularmovies.databinding.ActivityMainBinding;
 import com.example.android.popularmovies.ui.settings.SettingsActivity;
 import com.example.android.popularmovies.utility.InjectorUtils;
-
-import butterknife.BindString;
-import butterknife.BindView;
-import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -26,25 +21,24 @@ public class MainActivity extends AppCompatActivity {
     MoviesAdapter mMoviesAdapter;
     MainActivityViewModel mViewModel;
 
-    @BindView(R.id.activity_main_movies_recyclerView)
-    RecyclerView mMoviesRecyclerView;
-
-    @BindString(R.string.pref_sort_key) String mPrefSortKey;
-    @BindString(R.string.pref_sort_default) String mPrefSortDefault;
-
-    @BindString(R.string.error_displaying_movies) String mErrorDisplayingMovies;
+    //TODO: Remove Butterknife elements
+    //@BindView(R.id.activity_main_movies_recyclerView)
+    //RecyclerView mMoviesRecyclerView;
+    //@BindString(R.string.pref_sort_key) String mPrefSortKey;
+    //@BindString(R.string.pref_sort_default) String mPrefSortDefault;
+    //@BindString(R.string.error_displaying_movies) String mErrorDisplayingMovies;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ButterKnife.bind(this);
+        ActivityMainBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
 
-        mMoviesRecyclerView.setLayoutManager(new GridLayoutManager(getApplicationContext(), 2));
+        binding.activityMainMoviesRecyclerView.setLayoutManager(new GridLayoutManager(getApplicationContext(), 2));
 
-        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
-        String sortBy = prefs.getString(mPrefSortKey, mPrefSortDefault);
+        //SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        //String sortBy = prefs.getString(mPrefSortKey, mPrefSortDefault);
 
         MainActivityViewModelFactory factory = InjectorUtils.provideMainActivityViewModelFactory(
                 this.getApplicationContext());
