2bd27f295e62767881200951642b0ae7e5379e00
Amador Cuenca <sphi02ac@gmail.com>
2018-09-28 17:36:09 -0500

    feat: Pull movies into the MainActivity

--- app/src/main/java/com/example/android/popularmovies/ui/list/MainActivity.java
+++ app/src/main/java/com/example/android/popularmovies/ui/list/MainActivity.java
@@ -1,18 +1,25 @@
 package com.example.android.popularmovies.ui.list;
 
+import android.arch.lifecycle.Observer;
 import android.arch.lifecycle.ViewModelProviders;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.databinding.DataBindingUtil;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.preference.PreferenceManager;
 import android.support.v7.widget.GridLayoutManager;
 import android.view.Menu;
 import android.view.MenuItem;
 
 import com.example.android.popularmovies.R;
+import com.example.android.popularmovies.data.network.models.Movie;
 import com.example.android.popularmovies.databinding.ActivityMainBinding;
 import com.example.android.popularmovies.ui.settings.SettingsActivity;
 import com.example.android.popularmovies.utility.InjectorUtils;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,6 +27,8 @@ public class MainActivity extends AppCompatActivity {
 
     MoviesAdapter mMoviesAdapter;
     MainActivityViewModel mViewModel;
+
+    private int mCurrentPage = 1;
 
     //TODO: Remove Butterknife elements
     //@BindView(R.id.activity_main_movies_recyclerView)
@@ -37,12 +46,18 @@ public class MainActivity extends AppCompatActivity {
 
         binding.activityMainMoviesRecyclerView.setLayoutManager(new GridLayoutManager(getApplicationContext(), 2));
 
-        //SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
-        //String sortBy = prefs.getString(mPrefSortKey, mPrefSortDefault);
-
         MainActivityViewModelFactory factory = InjectorUtils.provideMainActivityViewModelFactory(
                 this.getApplicationContext());
         mViewModel = ViewModelProviders.of(this, factory).get(MainActivityViewModel.class);
+
+        mViewModel.getMovies().observeForever(new Observer<List<Movie>>() {
+            @Override
+            public void onChanged(@Nullable List<Movie> movieList) {
+                mMoviesAdapter.swapMovies(movieList);
+            }
+        });
+
+        swapMoviesInView();
     }
 
     @Override
@@ -62,5 +77,16 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private void swapMoviesInView() {
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        String sortBy = prefs.getString(getString(R.string.pref_sort_key), getString(R.string.pref_sort_default));
+
+        if (sortBy.equals(getString(R.string.pref_sort_default))) {
+            mViewModel.pullPopularMovies(mCurrentPage);
+        } else {
+            mViewModel.pullTopRatedMovies(mCurrentPage);
+        }
     }
 }
