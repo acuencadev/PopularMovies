945d5a65e7fb06ab4a620508f5ba6e595e9a0898
Amador Cuenca <sphi02ac@gmail.com>
2018-10-01 18:31:28 -0500

    feat: Add trailers list to the view model

--- app/src/main/java/com/example/android/popularmovies/ui/detail/DetailActivityViewModel.java
+++ app/src/main/java/com/example/android/popularmovies/ui/detail/DetailActivityViewModel.java
@@ -4,21 +4,29 @@ import android.arch.lifecycle.LiveData;
 import android.arch.lifecycle.ViewModel;
 
 import com.example.android.popularmovies.data.MoviesRepository;
-import com.example.android.popularmovies.data.database.MovieEntry;
-import com.example.android.popularmovies.data.database.MoviesDatabase;
 import com.example.android.popularmovies.data.network.models.Movie;
+import com.example.android.popularmovies.data.network.models.Trailer;
+
+import java.util.List;
 
 public class DetailActivityViewModel extends ViewModel {
 
     private LiveData<Movie> movie;
+    private LiveData<List<Trailer>> trailers;
     private final MoviesRepository repository;
 
     public DetailActivityViewModel(MoviesRepository repository, int movieId) {
         this.repository = repository;
         this.movie = repository.getMovie(movieId);
+        this.trailers = repository.getTrailers(movieId);
     }
 
     public LiveData<Movie> getMovie() {
         return this.movie;
+    }
+
+
+    public LiveData<List<Trailer>> getTrailers() {
+        return this.trailers;
     }
 }
