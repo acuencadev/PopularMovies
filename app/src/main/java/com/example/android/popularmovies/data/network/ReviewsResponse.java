74a9208e380d4a4d1298672ed406e3452ead5e5d
Amador Cuenca <sphi02ac@gmail.com>
2018-10-02 08:49:41 -0500

    feat: Add Review POJO and Reviews retrofit response

---
+++ app/src/main/java/com/example/android/popularmovies/data/network/ReviewsResponse.java
@@ -0,0 +1,63 @@
+package com.example.android.popularmovies.data.network;
+
+import com.example.android.popularmovies.data.network.models.Review;
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
+public class ReviewsResponse {
+
+    @SerializedName("id")
+    private int movieId;
+
+    @SerializedName("page")
+    private int page;
+
+    @SerializedName("total_pages")
+    private int totalPages;
+
+    @SerializedName("total_results")
+    private int totalResults;
+
+    private List<Review> results;
+
+    public int getMovieId() {
+        return movieId;
+    }
+
+    public void setMovieId(int movieId) {
+        this.movieId = movieId;
+    }
+
+    public int getPage() {
+        return page;
+    }
+
+    public void setPage(int page) {
+        this.page = page;
+    }
+
+    public List<Review> getResults() {
+        return results;
+    }
+
+    public void setResults(List<Review> results) {
+        this.results = results;
+    }
+
+    public int getTotalPages() {
+        return totalPages;
+    }
+
+    public void setTotalPages(int totalPages) {
+        this.totalPages = totalPages;
+    }
+
+    public int getTotalResults() {
+        return totalResults;
+    }
+
+    public void setTotalResults(int totalResults) {
+        this.totalResults = totalResults;
+    }
+}
