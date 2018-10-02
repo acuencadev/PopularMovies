5cee657e191a50813eb1b57432aa9e8ab3c65ed4
Amador Cuenca <sphi02ac@gmail.com>
2018-10-02 14:14:04 -0500

    feat: Add MovieReviewsFragment and ViewModel

---
+++ app/src/main/java/com/example/android/popularmovies/ui/detail/MovieReviewsFragment.java
@@ -0,0 +1,35 @@
+package com.example.android.popularmovies.ui.detail;
+
+import android.arch.lifecycle.ViewModelProviders;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.android.popularmovies.R;
+
+public class MovieReviewsFragment extends Fragment {
+
+    private MovieReviewsViewModel mViewModel;
+
+    public static MovieReviewsFragment newInstance() {
+        return new MovieReviewsFragment();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.movie_reviews_fragment, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = ViewModelProviders.of(this).get(MovieReviewsViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ app/src/main/java/com/example/android/popularmovies/ui/detail/MovieReviewsViewModel.java
@@ -0,0 +1,7 @@
+package com.example.android.popularmovies.ui.detail;
+
+import android.arch.lifecycle.ViewModel;
+
+public class MovieReviewsViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
