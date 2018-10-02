b523ec392254ca45dae31f136ef43d627c84e419
Amador Cuenca <sphi02ac@gmail.com>
2018-10-02 16:17:05 -0500

    feat: Load trailers into the fragment's RecyclerView

--- app/src/main/java/com/example/android/popularmovies/ui/detail/MovieTrailersFragment.java
+++ app/src/main/java/com/example/android/popularmovies/ui/detail/MovieTrailersFragment.java
@@ -1,19 +1,30 @@
 package com.example.android.popularmovies.ui.detail;
 
+import android.arch.lifecycle.Observer;
 import android.arch.lifecycle.ViewModelProviders;
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.android.popularmovies.R;
+import com.example.android.popularmovies.data.network.models.Trailer;
+import com.example.android.popularmovies.databinding.MovieTrailersFragmentBinding;
+import com.example.android.popularmovies.utility.InjectorUtils;
 
-public class MovieTrailersFragment extends Fragment {
+import java.util.List;
+
+public class MovieTrailersFragment extends Fragment
+        implements TrailersAdapter.OnItemClickListener {
 
     private MovieTrailersViewModel mViewModel;
+    private TrailersAdapter mTrailersAdapter;
+    private MovieTrailersFragmentBinding mBinding;
 
     public static MovieTrailersFragment newInstance() {
         return new MovieTrailersFragment();
@@ -22,14 +33,38 @@ public class MovieTrailersFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.movie_trailers_fragment, container, false);
+        mBinding = DataBindingUtil.inflate(inflater, R.layout.movie_trailers_fragment, container, false);
+        View view = mBinding.getRoot();
+
+        return view;
     }
 
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
-        mViewModel = ViewModelProviders.of(this).get(MovieTrailersViewModel.class);
-        // TODO: Use the ViewModel
+
+        mTrailersAdapter = new TrailersAdapter(getActivity(), this);
+
+        mBinding.activityDetailTrailersRecyclerView.setLayoutManager(
+                new LinearLayoutManager(getActivity().getApplicationContext()));
+        mBinding.activityDetailTrailersRecyclerView.setAdapter(mTrailersAdapter);
+        mBinding.activityDetailTrailersRecyclerView.setHasFixedSize(true);
+
+        MovieTrailersViewModelFactory factory = InjectorUtils.provideMovieTrailersViewModelFactory(
+                getActivity().getApplicationContext(), 550);
+
+        mViewModel = ViewModelProviders.of(this, factory).get(MovieTrailersViewModel.class);
+
+        mViewModel.getTrailers().observe(this, new Observer<List<Trailer>>() {
+            @Override
+            public void onChanged(@Nullable List<Trailer> trailers) {
+                mTrailersAdapter.swapTrailers(trailers);
+            }
+        });
     }
 
+    @Override
+    public void onItemClick(Trailer trailer) {
+        //TODO: Play trailer on Youtube or new browser window.
+    }
 }
