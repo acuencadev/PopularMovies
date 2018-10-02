95f84f63fca41e8943f49ffd73760784ff429218
Amador Cuenca <sphi02ac@gmail.com>
2018-10-02 17:16:15 -0500

    feat: Load reviews into the fragment's RecyclerView

--- app/src/main/java/com/example/android/popularmovies/ui/detail/MovieReviewsFragment.java
+++ app/src/main/java/com/example/android/popularmovies/ui/detail/MovieReviewsFragment.java
@@ -1,19 +1,30 @@
 package com.example.android.popularmovies.ui.detail;
 
+import android.arch.lifecycle.Observer;
 import android.arch.lifecycle.ViewModelProviders;
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.android.popularmovies.R;
+import com.example.android.popularmovies.data.network.models.Review;
+import com.example.android.popularmovies.databinding.MovieReviewsFragmentBinding;
+import com.example.android.popularmovies.utility.InjectorUtils;
 
-public class MovieReviewsFragment extends Fragment {
+import java.util.List;
+
+public class MovieReviewsFragment extends Fragment
+        implements ReviewsAdapter.OnItemClickListener {
 
     private MovieReviewsViewModel mViewModel;
+    private ReviewsAdapter mReviewsdapter;
+    private MovieReviewsFragmentBinding mBinding;
 
     public static MovieReviewsFragment newInstance() {
         return new MovieReviewsFragment();
@@ -22,14 +33,39 @@ public class MovieReviewsFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.movie_reviews_fragment, container, false);
+        mBinding = DataBindingUtil.inflate(inflater, R.layout.movie_reviews_fragment, container,
+                false);
+        View view = mBinding.getRoot();
+
+        return view;
     }
 
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
-        mViewModel = ViewModelProviders.of(this).get(MovieReviewsViewModel.class);
-        // TODO: Use the ViewModel
+
+        mReviewsdapter = new ReviewsAdapter(getActivity(), this);
+
+        mBinding.movieReviewsFragmentReviewsRecyclerView.setLayoutManager(
+                new LinearLayoutManager(getActivity().getApplicationContext()));
+        mBinding.movieReviewsFragmentReviewsRecyclerView.setAdapter(mReviewsdapter);
+        mBinding.movieReviewsFragmentReviewsRecyclerView.setHasFixedSize(true);
+
+        MovieReviewsViewModelFactory factory = InjectorUtils.provideMovieReviewsViewModelFactory(
+                getActivity().getApplicationContext(), 550, 1);
+
+        mViewModel = ViewModelProviders.of(this, factory).get(MovieReviewsViewModel.class);
+
+        mViewModel.getReviews().observe(this, new Observer<List<Review>>() {
+            @Override
+            public void onChanged(@Nullable List<Review> reviews) {
+                mReviewsdapter.swapTrailers(reviews);
+            }
+        });
     }
 
+    @Override
+    public void onItemClick(Review review) {
+        //TODO: Open review in The Movies DB site.
+    }
 }
