dc1837dad442558b442382558460b4f43f45de47
Amador Cuenca <sphi02ac@gmail.com>
2018-10-02 09:03:25 -0500

    feat: Add Reviews API call

--- app/src/main/java/com/example/android/popularmovies/data/network/MoviesApi.java
+++ app/src/main/java/com/example/android/popularmovies/data/network/MoviesApi.java
@@ -21,4 +21,6 @@ public interface MoviesApi {
     @GET("movie/{id}/videos")
     Call<TrailersResponse> getMovieTrailers(@Path("id") int id, @Query("api_key") String apiKey);
 
+    @GET("/movie/{id}/reviews")
+    Call<ReviewsResponse> getMovieReviews(@Path("id") int id, @Query("api_key") String apiKey, @Query("page") int page);
 }
