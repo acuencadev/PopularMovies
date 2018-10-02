7801bc16e788ecd484456052bf1e5e22f3d4d2dd
Amador Cuenca <sphi02ac@gmail.com>
2018-10-02 15:46:34 -0500

    feat: Load model into the ViewModel

--- app/src/main/java/com/example/android/popularmovies/ui/detail/MovieTrailersViewModel.java
+++ app/src/main/java/com/example/android/popularmovies/ui/detail/MovieTrailersViewModel.java
@@ -1,7 +1,25 @@
 package com.example.android.popularmovies.ui.detail;
 
+import android.arch.lifecycle.LiveData;
 import android.arch.lifecycle.ViewModel;
 
+import com.example.android.popularmovies.data.MoviesRepository;
+import com.example.android.popularmovies.data.network.models.Trailer;
+
+import java.util.List;
+
 public class MovieTrailersViewModel extends ViewModel {
-    // TODO: Implement the ViewModel
+
+    private final MoviesRepository mRepository;
+
+    private LiveData<List<Trailer>> mTrailers;
+
+    public MovieTrailersViewModel(MoviesRepository repository, int movieId) {
+        this.mRepository = repository;
+        this.mTrailers = this.mRepository.getTrailers(movieId);
+    }
+
+    public LiveData<List<Trailer>> getTrailers() {
+        return this.mTrailers;
+    }
 }
---
+++ app/src/main/java/com/example/android/popularmovies/ui/detail/MovieTrailersViewModelFactory.java
@@ -0,0 +1,25 @@
+package com.example.android.popularmovies.ui.detail;
+
+import android.arch.lifecycle.ViewModel;
+import android.arch.lifecycle.ViewModelProvider;
+import android.support.annotation.NonNull;
+
+import com.example.android.popularmovies.data.MoviesRepository;
+
+public class MovieTrailersViewModelFactory extends ViewModelProvider.NewInstanceFactory {
+
+    private final MoviesRepository mRepository;
+    private final int mMovieId;
+
+
+    public MovieTrailersViewModelFactory(MoviesRepository mRepository, int movieId) {
+        this.mMovieId = movieId;
+        this.mRepository = mRepository;
+    }
+
+    @NonNull
+    @Override
+    public <T extends ViewModel> T create(@NonNull Class<T> modelClass) {
+        return (T) new DetailActivityViewModel(mRepository, mMovieId);
+    }
+}
