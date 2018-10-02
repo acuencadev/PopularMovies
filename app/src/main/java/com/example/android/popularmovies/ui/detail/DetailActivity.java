f2137c0354f3fdbca25615ec4b4b960d5f7534fe
Amador Cuenca <sphi02ac@gmail.com>
2018-10-02 17:51:32 -0500

    feat: Pass Movie ID to the fragment from the activity

--- app/src/main/java/com/example/android/popularmovies/ui/detail/DetailActivity.java
+++ app/src/main/java/com/example/android/popularmovies/ui/detail/DetailActivity.java
@@ -43,7 +43,7 @@ public class DetailActivity extends AppCompatActivity {
 
     private void setupViewPager(ViewPager viewPager) {
         ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
-        adapter.addFragment(new MovieDescriptionFragment(), getString(R.string.tab_movies_description));
+        adapter.addFragment(MovieDescriptionFragment.newInstance(mMovieId), getString(R.string.tab_movies_description));
         adapter.addFragment(MovieTrailersFragment.newInstance(mMovieId), getString(R.string.tab_movies_trailers));
         adapter.addFragment(MovieReviewsFragment.newInstance(mMovieId), getString(R.string.tab_movies_reviews));
         viewPager.setAdapter(adapter);
