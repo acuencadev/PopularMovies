bf2b2139e9a505a6a67014f96d15df5906f01a45
Amador Cuenca <sphi02ac@gmail.com>
2018-10-02 14:11:10 -0500

    feat: Add MovieDescriptionFragment and ViewModel

---
+++ app/src/main/java/com/example/android/popularmovies/ui/detail/MovieDescriptionFragment.java
@@ -0,0 +1,35 @@
+package com.example.android.popularmovies.ui.detail;
+
+import android.arch.lifecycle.ViewModelProviders;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.android.popularmovies.R;
+
+public class MovieDescriptionFragment extends Fragment {
+
+    private MovieDescriptionViewModel mViewModel;
+
+    public static MovieDescriptionFragment newInstance() {
+        return new MovieDescriptionFragment();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.movie_description_fragment, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = ViewModelProviders.of(this).get(MovieDescriptionViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
