d41687acffc7ea6c7095cdefe4d39e2cbe46ef6e
Amador Cuenca <sphi02ac@gmail.com>
2018-10-02 16:41:58 -0500

    feat: Load model into the Reviews ViewModel

--- app/src/main/java/com/example/android/popularmovies/ui/detail/MovieReviewsViewModel.java
+++ app/src/main/java/com/example/android/popularmovies/ui/detail/MovieReviewsViewModel.java
@@ -1,7 +1,25 @@
 package com.example.android.popularmovies.ui.detail;
 
+import android.arch.lifecycle.LiveData;
 import android.arch.lifecycle.ViewModel;
 
+import com.example.android.popularmovies.data.MoviesRepository;
+import com.example.android.popularmovies.data.network.models.Review;
+
+import java.util.List;
+
 public class MovieReviewsViewModel extends ViewModel {
-    // TODO: Implement the ViewModel
+
+    private final MoviesRepository mRepository;
+
+    private LiveData<List<Review>> mReviews;
+
+    public MovieReviewsViewModel(MoviesRepository repository, int movieId, int page) {
+        this.mRepository = repository;
+        this.mReviews = this.mRepository.getReviews(movieId, page);
+    }
+
+    public LiveData<List<Review>> getReviews() {
+        return this.mReviews;
+    }
 }
---
+++ app/src/main/java/com/example/android/popularmovies/ui/detail/MovieReviewsViewModelFactory.java
@@ -0,0 +1,25 @@
+package com.example.android.popularmovies.ui.detail;
+
+import android.arch.lifecycle.ViewModel;
+import android.arch.lifecycle.ViewModelProvider;
+import android.support.annotation.NonNull;
+
+import com.example.android.popularmovies.data.MoviesRepository;
+
+public class MovieReviewsViewModelFactory extends ViewModelProvider.NewInstanceFactory {
+
+    private final MoviesRepository mRepository;
+    private final int mMovieId;
+    private final int mPage;
+
+    public MovieReviewsViewModelFactory(MoviesRepository mRepository, int movieId, int page) {
+        this.mPage = page;
+        this.mMovieId = movieId;
+        this.mRepository = mRepository;
+    }
+    @NonNull
+    @Override
+    public <T extends ViewModel> T create(@NonNull Class<T> modelClass) {
+        return (T) new MovieReviewsViewModel(mRepository, mMovieId, mPage);
+    }
+}
