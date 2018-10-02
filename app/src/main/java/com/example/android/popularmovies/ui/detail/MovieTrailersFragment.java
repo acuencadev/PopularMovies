bbd678108b24117c5b43fc45873a9d559575075d
Amador Cuenca <sphi02ac@gmail.com>
2018-10-02 14:12:30 -0500

    feat: Add MovieTrailersFragment and ViewModel

---
+++ app/src/main/java/com/example/android/popularmovies/ui/detail/MovieTrailersFragment.java
@@ -0,0 +1,35 @@
+package com.example.android.popularmovies.ui.detail;
+
+import android.arch.lifecycle.ViewModelProviders;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.android.popularmovies.R;
+
+public class MovieTrailersFragment extends Fragment {
+
+    private MovieTrailersViewModel mViewModel;
+
+    public static MovieTrailersFragment newInstance() {
+        return new MovieTrailersFragment();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.movie_trailers_fragment, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = ViewModelProviders.of(this).get(MovieTrailersViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
